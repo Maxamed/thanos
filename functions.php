82267970053a02ee3867d5dc228743aa231bf5ed
Mohamed Jama <mj@mjama.com>
2014-11-11 17:59:16 +0000

    done the hastag system

--- functions.php
+++ functions.php
@@ -15,12 +15,13 @@ if(isset($_POST['submit']))
  
          $data = array(
             'posts' => $_POST['post'], 
+            'hashtag'=> gethashtags($_POST['post']),
             'category' => $_POST['category'], 
             'status'=> 1,
             'createdat' => $db->now()
         );
 
-       
+//var_dump( $data);       
         $id = $db->insert('posts', $data);
 
         $dataX = array(
@@ -65,13 +66,59 @@ function printPosts() {
     $db->where ("status", true);
     $posts = $db->get("posts");
     foreach ($posts as $u) {
-        
-        $post = truncate($u['posts']);
-        echo "  <li><a href=post.php?SingleId={$u['id']}>Post: {$post}<span> -  - ID:{$u['id']}</span><span>Category:{$u['category']}</span><p><a href=functions.php?SpamId={$u['id']}>Report Spam</a></p></li>";
+        $hashPost = convertHashtags($u['posts']);
+        $post = truncate($hashPost );
+        echo "  <li>Post: {$post} <a href=post.php?SingleId={$u['id']}><br><span>ID:{$u['id']}</span> - <span>Category: {$u['category']}</span><p><a href=functions.php?SpamId={$u['id']}>Report Spam</a></p></li>";
     }
 }
 
-//  TODO  Spam
+
+//Strip and store Hashtag function
+ 
+function gethashtags($text)
+{
+      //Match the hashtags
+      preg_match_all('/(^|[^a-z0-9_])#([a-z0-9_]+)/i', $text, $matchedHashtags);
+      $hashtag = '';
+      // For each hashtag, strip all characters but alpha numeric
+      if(!empty($matchedHashtags[0])) {
+          foreach($matchedHashtags[0] as $match) {
+              $hashtag .= preg_replace("/[^a-z0-9]+/i", "", $match).',';
+          }
+      }
+    //to remove last comma in a string
+    return rtrim($hashtag, ',');
+}
+
+// Convert to hastag function
+function convertHashtags($str){
+    $regex = "/#+([a-zA-Z0-9_]+)/";
+    $str = preg_replace($regex, '<a href="functions.php?tag=$1">$0</a>', $str);
+    return($str);
+}
+
+//Pocess hashtags
+
+if(isset($_GET['tag'])){
+    $r = $_GET["tag"];
+    $db->where("hashtag", Array ('LIKE' => '%'.$r.'%'));
+    $posts = $db->get("posts");
+    if ($db->count < 1) {
+        echo "Invalid insert count in LIKE: ".$db->count;
+        print_r ($posts);
+        echo $db->getLastQuery();
+        exit;
+    }else { 
+            foreach ($posts as $u) {
+                $hashPost = convertHashtags($u['posts']);
+                $post = truncate($hashPost );
+                echo "  <li>Post: {$post} <a href=post.php?SingleId={$u['id']}><br><span>ID:{$u['id']}</span> - <span>Category: {$u['category']}</span><p><a href=functions.php?SpamId={$u['id']}>Report Spam</a></p></li>";
+            }
+    }
+
+ 
+}
+//  Spam
 
 if(isset($_GET['SpamId']))
     { 
