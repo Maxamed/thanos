a6ee56ada38fe7c44808a2f329b5739b6356bd2e
Mohamed Jama <mj@mjama.com>
2015-01-04 19:45:42 +0000

    timestampe shit pt3

--- app/gauntlet.php
+++ app/gauntlet.php
@@ -17,7 +17,7 @@ function sPost($id){
     $db->where ("id", $t); 
     $u = $db->getOne("posts"); 
 
-    $dateee = date('l jS \of F Y h:i:s A',strtotime($u['createdat']));
+    $dateee = date('jS \of F Y h:i:s A',strtotime($u['createdat']));
     $arrPosts[] = array(
         "posts" => array(
                 "id"        => $u['id'],
@@ -35,7 +35,7 @@ function sPost($id){
 
     foreach ($comments as $x) {
 
-        $datee = date('l jS \of F Y h:i:s A',strtotime($u['createdat']));
+        $datee = date('jS \of F Y h:i:s A',strtotime($u['createdat']));
         $arrPosts['comments'][] = array( 
             "id"        => $x['id'], 
             "comment"   => $x['comment'],
@@ -61,7 +61,7 @@ function allPosts() {
 
     foreach ($posts as $u) {
         
-        $datee = date('l jS \of F Y h:i:s A',strtotime($u['createdat']));
+        $datee = date('jS \of F Y h:i:s A',strtotime($u['createdat']));
 
         $hashPost = convertHashtags($u['posts']);
         $post = truncate($hashPost); 
@@ -94,7 +94,7 @@ function allComments($id) {
     $allComms = array();
 
     foreach ($comments as $u) {
-        $datee = date('l jS \of F Y h:i:s A',strtotime($u['createdat']));
+        $datee = date('jS \of F Y h:i:s A',strtotime($u['createdat']));
 
         $allComms[] = array(
 
@@ -135,7 +135,7 @@ function allComments($id) {
         );
         foreach ($posts as $u) {
 
-            $datee = date('l jS \of F Y h:i:s A',strtotime($u['createdat']));
+            $datee = date('jS \of F Y h:i:s A',strtotime($u['createdat']));
 
             $hashPost = convertHashtags($u['posts']);
             $post = truncate($hashPost); 
